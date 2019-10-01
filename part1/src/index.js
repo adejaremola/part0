702c4655e1d5a7aa8f7454e7f83af9f765889f01
AbdulQudus Adebogun <adejaremola@gmail.com>
2019-10-01 22:23:21 +0100

    complete Course Information step3-5; refactor the components to use objects and arrays

--- part1/src/index.js
+++ part1/src/index.js
@@ -9,54 +9,57 @@ const Part = (props) => (
     <p>{ props.part } { props.exercises }</p>
 );
 
-const Content = (props) => (
+const Content = props => (
     <>
         <Part 
-            part={ props.part1 }
-            exercises={ props.exercises1 }
+            part={ props.parts[0].name }
+            exercises={ props.parts[0].exercises }
         />
         <Part 
-            part={ props.part2 }
-            exercises={ props.exercises2 }
+            part={ props.parts[1].name }
+            exercises={ props.parts[1].exercises }
         />
         <Part 
-            part={ props.part3 }
-            exercises={ props.exercises3 }
+            part={ props.parts[2].name }
+            exercises={ props.parts[2].exercises }
         />
     </>
 );
 
-const Total = (props) => (
-    <p>Number of exercises { props.exercises1 + props.exercises2 + props.exercises3 }</p>
+const Total = props => (
+    <p>Number of exercises { props.exercises[0].exercises + props.exercises[1].exercises + props.exercises[2].exercises }</p>
 );
 
 const App = () => {
-    const 
-        course = 'Half Stack application development', 
-        part1 = 'Fundamentals of React',
-        exercises1 = 10,
-        part2 = 'Using props to pass data',
-        exercises2 = 7,
-        part3 = 'State of a component', 
-        exercises3 = 14;
+    const course = {
+        name: 'Half Stack application development', 
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component', 
+                exercises: 14
+            }
+        ]
+    };
     
     return (
         <div>
-            <Header course={ course } />
+            <Header course={ course.name } />
             <Content 
-                part1={ part1 }
-                part2={ part2 }
-                part3={ part3 }
-                exercises1={ exercises1 }
-                exercises2={ exercises2 }
-                exercises3={ exercises3 }
+                parts={ course.parts }
             />
             <Total 
-                exercises1={ exercises1 }
-                exercises2={ exercises2 }
-                exercises3={ exercises3 }
+                exercises={ course.parts }
             />
         </div>
     )
 }
+
 ReactDOM.render(<App />, document.getElementById('root'));
