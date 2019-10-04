0f420a8c665bcf2014210cfb8937e5feec271b44
AbdulQudus Adebogun <adejaremola@gmail.com>
2019-10-04 04:10:14 +0100

    Implement part1.12 to part1.14*

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,33 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = ({ course }) => <h1>{ course }</h1> ;
-
-const App = () => {
+const App = ({ anecdotes }) => {
     const 
-        course = 'Half Stack application development',
-        part1 = 'Fundamentals of React',
-        exercises1 = 10,
-        part2 = 'Using props to pass data',
-        exercises2 = 7,
-        part3 = 'State of a component',
-        exercises3 = 14;
+        [selected, setSelected] = useState(0),
+        [votes, setVotes] = useState([0, 0, 0, 0, 0, 0]),
+        
+        handleNextAnec = () => {
+            let randIndex = Math.round(Math.random() * (anecdotes.length - 1));
+            setSelected(randIndex);
+        },
+        
+        handleVoteAnec = () => {
+            const copy = [ ...votes ];
+            copy[selected] += 1; 
+            setVotes(copy);
+        };
+        
+    let votesCopy = [...votes];
+    votesCopy.sort((a, b) => b - a);
+    let highestVotes = votesCopy[0];
+    let highestAnecIndex = votes.indexOf(highestVotes);
 
     return (
-        <div>
-            <Header course={ course } />
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
-            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-        </div>
-    )
+        <>
+            <h2>Anecdote of the day</h2>
+            <div>
+                {anecdotes[selected]} 
+                <br/>
+                has { votes[selected] } votes.
+            </div>
+            <button onClick={ handleVoteAnec }>vote</button>
+            <button onClick={ handleNextAnec }>next anecdote</button>
+            <h2>Anecdote with most votes</h2>
+            <div>
+                {anecdotes[highestAnecIndex]} 
+                <br/>
+                has { votes[highestAnecIndex] } votes.
+            </div>
+        </>
+    );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+    <App anecdotes={anecdotes} />, document.getElementById('root')
+)
--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -26,7 +26,7 @@ const Content = ({ course }) => {
 };
 
 const Total = ({ course }) => {
-    const {part1, part2, part3} = course.parts;
+    const [part1, part2, part3] = course.parts;
     return (
         <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
     )
